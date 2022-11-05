bce33d4931746e65a84183fc7f1957ac9ad6b359
drkittykatt <vixenaboutique@gmail.com>
2022-11-04 19:53:12 -0700

    update

--- packages/my-server/routers/authRouter.js
+++ packages/my-server/routers/authRouter.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const validateForm = require("../controllers/validateForm");
 const router = express.Router();
-
+const pool = require("../db");
+const bcrypt = require("bcrypt");
 
 router.post("/login", (req, res) => {
   validateForm(req, res);
 });
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   validateForm(req, res);
+
+  const existingUser = await pool.query(
+    "SELECT username from users WHERE username=$1",
+    [req.body.username]
+  );
+
+  if (existingUser.rowCount === 0) {
+    // register
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    const newUserQuery = await pool.query(
+      "INSERT INTO users(username, passhash) values($1,$2) RETURNING id, username",
+      [req.body.username, hashedPass]
+    );
+    req.session.user = {
+      username: req.body.username,
+      id: newUserQuery.rows[0].id,
+    };
+
+    res.json({ loggedIn: true, username: req.body.username });
+  } else {
+    res.json({ loggedIn: false, status: "Username taken" });
+  }
 });
 module.exports = router;
