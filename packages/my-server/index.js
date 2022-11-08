09be94682e3f441394c9d155d9a0b9bf635a6688
drkittykatt <vixenaboutique@gmail.com>
2022-11-07 18:21:38 -0800

    add sessions

--- packages/my-server/index.js
+++ packages/my-server/index.js
@@ -3,8 +3,9 @@ const helmet = require("helmet");
 const { Server } = require("socket.io"); //do i need this? 
 const app = express();
 const cors = require("cors");
-const authRouter = require("./routers/authRouter")
-
+const authRouter = require("./routers/authRouter");
+const session = require("express-session");
+require("dotenv").config()
 const server = require("http").createServer(app);
 
 const io = new Server(server, {
@@ -20,7 +21,18 @@ app.use(cors({
     credentials: true,
 }))
 app.use(express.json());
-
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    credentials: true,
+    name: "sid",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.ENVIRONMENT === "production",
+        httpOnly: true,
+        sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
+    }
+}))
 app.use("/auth", authRouter)
 
 // app.get("/api", (req, res) => {
