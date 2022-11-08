fd525f81e4a028203c2848250d818807ffa1ed92
drkittykatt <vixenaboutique@gmail.com>
2022-11-07 19:14:06 -0800

    I broke something

--- packages/my-server/routers/authRouter.js
+++ packages/my-server/routers/authRouter.js
@@ -4,9 +4,42 @@ const router = express.Router();
 const pool = require("../db");
 const bcrypt = require("bcrypt");
 
-router.post("/login", (req, res) => {
-  validateForm(req, res);
-});
+router.route("/login")
+  .get(async (req, res) => {
+    if (req.session.user && req.session.user.username) {
+      res.json({ loggedIn: true, username: req.session.user.username });
+    } else {
+      res.json({ loggedIn: false });
+    }
+  })
+  .post(async (req, res) => {
+    validateForm(req, res);
+
+    const potentialLogin = await pool.query(
+      "SELECT id, username, passhash FROM users u WHERE u.username=$1",
+      [req.body.username]
+    );
+
+    if (potentialLogin.rowCount > 0) {
+      const isSamePass = await bcrypt.compare(
+        req.body.password,
+        potentialLogin.rows[0].passhash
+      );
+      if (isSamePass) {
+        req.session.user = {
+          username: req.body.username,
+          id: potentialLogin.rows[0].id,
+        };
+        res.json({ loggedIn: true, username: req.body.username });
+      } else {
+        res.json({ loggedIn: false, status: "Wrong username or password!" });
+        console.log("not good");
+      }
+    } else {
+      console.log("not good");
+      res.json({ loggedIn: false, status: "Wrong username or password!" });
+    }
+  });
 
 router.post("/signup", async (req, res) => {
   validateForm(req, res);
